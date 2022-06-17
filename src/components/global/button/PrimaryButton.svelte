f4d1bbb9af31041e03b5a50c3be336edb05cad0b
ilzam <ilzammulkhaq85@gmail.com>
2022-06-17 21:12:14 +0700

    feat: adding PrimaryButton component

---
+++ src/components/global/button/PrimaryButton.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import classnames from "classnames";
+
+    const dispatch = createEventDispatcher();
+
+    export let isSubmit = false;
+    export let skin = "primary";
+    export let wide = false;
+    export let disable = false;
+    export let loading = false;
+
+    let onButtonClicked = () => {
+        dispatch("click");
+    };
+
+    let btnClass = classnames("btn", `btn-${skin}`, {
+        "w-full": wide,
+        disabled: disable,
+    });
+</script>
+
+<button
+    type={isSubmit ? "submit" : "button"}
+    class={btnClass}
+    on:click={onButtonClicked}
+    disabled={disable}
+>
+    {#if loading}
+        <i class="bx bx-loader-alt bx-spin mr-1" />
+    {/if}
+    <slot name="children" />
+</button>
